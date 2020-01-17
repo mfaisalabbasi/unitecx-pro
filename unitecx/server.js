806da37a23341c024bbae8f96fa46b283abd47e9
Faisal <faisal-mfaisalabbasi0@gmail.com>
2020-01-17 16:32:41 +0500

    upadting

--- unitecx/server.js
+++ unitecx/server.js
@@ -2,19 +2,24 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const connectDb = require('./config/db');
+const path = require('path');
 
+const PORT = process.env.PORT || 5000;
 connectDb();
 app.use(cors());
 app.use(express.json({ extended: false }));
 app.use(express.urlencoded({ extended: false }));
 app.use('/public', express.static('public'));
-app.get('/', (req, res) => {
-  res.status(200).json(`Welcome to Restful Api's`);
-});
 
 app.use('/api/projects', require('./routes/api/projects'));
 app.use('/api/portfolios', require('./routes/api/portfolios'));
 app.use('/api/services', require('./routes/api/services'));
 app.use('/api/user', require('./routes/api/user'));
-const PORT = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dir, 'client', 'build', 'index.html'));
+  });
+}
 app.listen(PORT, () => console.log(`app is ruunig on port ${PORT}`));
