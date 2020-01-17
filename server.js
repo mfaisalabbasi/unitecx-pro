32ded0b55e9760c2ae1cfc26e70c6d4408d5b65c
Faisal <faisal-mfaisalabbasi0@gmail.com>
2020-01-17 20:43:48 +0500

    bat ba

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const app = express();
 const connectDb = require('./config/db');
 const path = require('path');
 
-const PORT = process.env.PORT || 5000;
 connectDb();
 app.use(cors());
 app.use(express.json({ extended: false }));
